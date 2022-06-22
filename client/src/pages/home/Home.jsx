b342dc5b761268ecf1fdf915f04946591e60bc6e
MkMonir <khanmonir1335@gmail.com>
2022-06-22 18:50:44 +0600

    create netflix home page

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Navbar from '../../components/navbar/Navbar';
+import Featured from '../../components/featured/Featured';
+import List from '../../components/list/List';
+import './home.scss';
+
+const Home = ({ type }) => {
+  const [lists, setLists] = useState([]);
+  const [genre, setGenre] = useState(null);
+
+  useEffect(() => {
+    const getRandomList = async () => {
+      try {
+        const res = await axios.get(
+          `http://localhost:8800/api/v1/lists${type ? '?type=' + type : ''}${
+            genre ? '&genre=' + genre : ''
+          }`,
+          {
+            headers: {
+              token: JSON.parse(localStorage.getItem('user')).token,
+              'Content-Type': 'application/json',
+            },
+          }
+        );
+
+        setLists(res.data.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getRandomList();
+  }, [type, genre]);
+
+  return (
+    <div className="home">
+      <Navbar />
+      <Featured type={type} setGenre={setGenre} />
+      {lists.map((list) => (
+        <List key={list._id} list={list} />
+      ))}
+    </div>
+  );
+};
+
+export default Home;
