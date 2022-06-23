12e00492d54a1119318d926e6695d9232e652c05
MkMonir <khanmonir1335@gmail.com>
2022-06-23 14:06:42 +0600

    create context, reducer and apiCalls for authentication

---
+++ admin/src/contexts/authContext/apiCalls.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { loginStart, loginSuccess, loginFailure } from './AuthActions';
+
+export const login = async (user, dispatch) => {
+  dispatch(loginStart());
+
+  try {
+    const res = await axios.post(`http://localhost:8800/api/v1/auth/login`, user, {
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    console.log(res.data);
+    res.data.data.user.isAdmin && dispatch(loginSuccess(res.data));
+  } catch (err) {
+    dispatch(loginFailure());
+  }
+};
