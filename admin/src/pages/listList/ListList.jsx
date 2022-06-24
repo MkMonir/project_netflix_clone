7539a0cf9eb59adbad2c5a957e9c3989a88ae0b6
MkMonir <khanmonir1335@gmail.com>
2022-06-24 21:58:08 +0600

    create lists page and fetch lists

---
+++ admin/src/pages/listList/ListList.jsx
@@ -0,0 +1,61 @@
+import { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './listList.css';
+import { DataGrid } from '@material-ui/data-grid';
+import { DeleteOutline } from '@material-ui/icons';
+import { ListContext } from './../../contexts/listContext/ListContext';
+import { deleteList, getLists } from '../../contexts/listContext/apiCalls';
+
+export default function ListList() {
+  const { lists, dispatch } = useContext(ListContext);
+
+  useEffect(() => {
+    getLists(dispatch);
+  }, [dispatch]);
+
+  const handleDelete = (id) => {
+    deleteList(id, dispatch);
+  };
+
+  const columns = [
+    { field: '_id', headerName: 'ID', width: 250 },
+    { field: 'title', headerName: 'Title', width: 250 },
+    { field: 'genre', headerName: 'Genre', width: 150 },
+    { field: 'type', headerName: 'Type', width: 150 },
+
+    {
+      field: 'action',
+      headerName: 'Action',
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={{ pathname: '/list/' + params.row._id, list: params.row }}>
+              <button className="productListEdit">Edit</button>
+            </Link>
+            <DeleteOutline
+              className="productListDelete"
+              onClick={() => handleDelete(params.row._id)}
+            />
+          </>
+        );
+      },
+    },
+  ];
+
+  return (
+    <div className="productList">
+      <Link to="/newlist" className="productAddButtonLink">
+        <button className="productAddButton">Create List</button>
+      </Link>
+      <DataGrid
+        rows={lists}
+        disableSelectionOnClick
+        columns={columns}
+        pageSize={8}
+        checkboxSelection
+        getRowId={(r) => r._id}
+      />
+    </div>
+  );
+}
