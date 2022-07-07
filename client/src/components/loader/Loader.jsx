382732dd85787f5dd6b8de60271f865ed82a04f1
MkMonir <khanmonir1335@gmail.com>
2022-07-07 16:56:34 +0600

    loader component

---
+++ client/src/components/loader/Loader.jsx
@@ -0,0 +1,10 @@
+import './loader.css';
+
+const Loader = () => {
+  return (
+    <div className="loaderContainer">
+      <div id="loader" className="nfLoader"></div>
+    </div>
+  );
+};
+export default Loader;
